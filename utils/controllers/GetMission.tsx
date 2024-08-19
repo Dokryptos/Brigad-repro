310bef81b56ee2b71f708d01a26230784a2e1ac2
Pierre Dok <pierredok@outlook.fr>
2024-08-19 20:16:15 +0200

    new organisation more clean

--- utils/controllers/GetMission.tsx
+++
@@ -1,5 +0,0 @@
-// import { MissionItem } from '../types/MissionItem'
-
-// export const getMission = async (query: string): Promise<MissionItem[] | []> => {
-
-// }
--- utils/supabase/supabaseClient.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://rnixyvahdrgnbbqbefes.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
